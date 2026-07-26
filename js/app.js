5b29c38e3e872600a40dab8d4d38241f5fc4ec7b
khanhhhuyenpt <khanhhhuyenp.t@gmail.com>
2026-07-27 00:55:15 +0700

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*
+========================================
+Ms. Huyen's Hub
+Main JavaScript File
+Version: 1.0
+========================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("🦊 Welcome to Ms. Huyen's Hub!");
+});
